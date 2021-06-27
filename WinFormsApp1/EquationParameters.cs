6852bd49ad14b1222880857c930afd8b202a8a94
Kpro <44187035+cravens1968@users.noreply.github.com>
2021-06-27 07:36:33 -0700

    refactored. But can't figure out how to add controls.

---
+++ WinFormsApp1/EquationParameters.cs
@@ -0,0 +1,17 @@
+namespace howto_histogram
+{
+    public class EquationParameters
+    {
+        public EquationParameters(float a, float b, float c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public float A { get; set; }
+        public float B { get; set; }
+        public float C { get; set; }
+        
+    }
+}
--- WinFormsApp1/WeightedRandom.cs
+++ WinFormsApp1/WeightedRandom.cs
@@ -5,11 +5,23 @@ namespace howto_histogram
     public class WeightedRandom : Random
     {
             // adjusts the normal curve toward the right (higher) end depending on number of chargesdused. 
-            public float Weight { get; set; }
+            public WeightedRandom(double _A, double _B, double _C, double _D) : base()
+            {
+                A = _A;
+                B = _B;
+                C = _C;
+                D = _D;
+            }
+
+            public double A { get; set; }
+            public double B { get; set; }
+            public double C { get; set; }
+            public double D { get; set; }
 
             protected override double Sample()
             {
-                return Math.Pow(base.Sample(),Weight);
+                double x = base.Sample();
+                return A * x * x * x * x + B * x * x * x + C * x * x + D * x;
             }
             public override int Next(int MaxValue)
             {
